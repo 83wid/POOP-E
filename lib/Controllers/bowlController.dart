adf4f96210b17a90c006c8fe4bb8b3667cc9927e
83wid <nabouzah@student.1337.ma>
2021-08-26 23:18:28 +0100

    added assets gen

--- lib/Controllers/bowlController.dart
+++ lib/Controllers/bowlController.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 List<String> type = [
   'Separated hard lumps',
   'Lumpy and sausage like',
@@ -7,6 +9,15 @@ List<String> type = [
   'Mushy with ragged edges',
   'Liquid, with no solid pieces'
 ];
+List<String> imgtype = [
+ 'assets/images/type1.png',
+ 'assets/images/type2.png',
+ 'assets/images/type3.png',
+ 'assets/images/type4.png',
+ 'assets/images/type5.png',
+ 'assets/images/type6.png',
+ 'assets/images/type7.png',
+];
 List<String> color = [
   'Darkned',
   'Dark Brown',
@@ -15,6 +26,14 @@ List<String> color = [
   'Little Green',
   'Green',
 ];
+List<String> imgcolor = [
+ 'assets/images/darkned.png',
+ 'assets/images/darkBrown.png',
+ 'assets/images/brown.png',
+ 'assets/images/lightBrown.png',
+ 'assets/images/littleGreen.png',
+ 'assets/images/green.png',
+];
 List<String> smell = [
   'Smelless',
   'Soft Smell',
@@ -22,17 +41,38 @@ List<String> smell = [
   'Strong Smell',
   'Unbearable Smell',
 ];
+List <String> imgsmell = [
+ 'assets/images/smelless.png',
+ 'assets/images/soft.png',
+ 'assets/images/moderatesmell.png',
+ 'assets/images/strongSmell.png',
+ 'assets/images/unbearaleSmell.png',
+];
 List<String> volume = [
   'Less than normal',
   'Normal',
   'More than normal',
 ];
+List <String> imgvolume = [
+  'assets/images/less.png',
+  'assets/images/normal.png',
+  'assets/images/more.png',
+];
+
 List<String> pain = [
   'No pain',
   'Mild pain',
   'Moderate pain',
   'Intense pain',
   'Unbearable pain',
+];
+
+List <String> imgpain = [
+  'assets/images/noPain.png',
+  'assets/images/mildPain.png',
+  'assets/images/moderatePain.png',
+  'assets/images/intensePain.png',
+  'assets/images/unbearablePain.png',
 ];
 List<String> symptoms = [
   'Dizziness',
@@ -43,7 +83,16 @@ List<String> symptoms = [
   'Chills',
   'Urgency',
 ];
-List<String> duration = ['5 min', '15 min', '10 min', '20 min', '25 min'];
+List<String> duration = ['5 min', '10 min', '15 min', '20 min', '25 min'];
+
+List <String> imgduration = [
+  'assets/images/5min.png',
+  'assets/images/10min.png',
+  'assets/images/15min.png',
+  'assets/images/20min.png',
+  'assets/images/25min.png',
+];
+
 bool blood = false;
 bool floatiness = false;
 bool flalulence = false;
