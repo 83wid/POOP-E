ab3eb1fde059ce28b53d0a20fe135554ef2bc5fd
83wid <nabouzah@student.1337.ma>
2021-09-05 21:47:02 +0100

    updated deprecated methods

--- lib/utilities/notificationManager.dart
+++ lib/utilities/notificationManager.dart
@@ -1,15 +1,22 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 
 class NotificationManager {
-  var flutterLocalNotificationsPlugin;
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   NotificationManager() {
     flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
     initNotifications();
   }
 
-  getNotificationInstance() {
+  FlutterLocalNotificationsPlugin getNotificationInstance() {
     return flutterLocalNotificationsPlugin;
+  }
+
+  Future configureLocalTime(dateTime) async {
+    tz.initializeTimeZones();
+    return tz.TZDateTime.from(dateTime, tz.getLocation('Africa/Casablanca'));
   }
 
   void initNotifications() {
@@ -28,10 +35,26 @@ class NotificationManager {
 
   void showNotificationDaily(
       int id, String? title, String? body, int hour, int minute) async {
-    var time = new Time(hour, minute, 0);
-    await flutterLocalNotificationsPlugin.showDailyAtTime(
-        id, title, body, time, getPlatformChannelSpecfics());
-    print('Notification Succesfully Scheduled at ${time.hour.toString() + ': ' + time.minute.toString()}');
+    var dateTime = new DateTime(DateTime.now().year, DateTime.now().month,
+        DateTime.now().day, hour, minute, 0);
+
+    // The device's timezone.
+
+    final tz.TZDateTime scheduledDate = await configureLocalTime(dateTime);
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+     scheduledDate,
+      getPlatformChannelSpecfics(),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+    // await flutterLocalNotificationsPlugin.showDailyAtTime(
+    //     id, title, body, time, getPlatformChannelSpecfics());
+    print(
+        'Notification Succesfully Scheduled at ${dateTime.hour.toString() + ': ' + dateTime.minute.toString()}');
   }
 
   getPlatformChannelSpecfics() {
@@ -48,9 +71,9 @@ class NotificationManager {
     return platformChannelSpecifics;
   }
 
-  Future <dynamic> onSelectNotification(String? payload) async {
-    print('Notification clicked');
-    return Future.value(0);
+  Future<dynamic> onSelectNotification(String? payload) async {
+    print('Hello it\'s pressed');
+    return Future.value(1);
   }
 
   Future onDidReceiveLocalNotification(
@@ -60,5 +83,36 @@ class NotificationManager {
 
   void removeReminder(int notificationId) {
     flutterLocalNotificationsPlugin.cancel(notificationId);
+  }
+
+  Future<void> scheduleNotification(
+      {int id = 0,
+      String? title,
+      String? body,
+      DateTime? scheduledTime}) async {
+    var androidSpecifics = AndroidNotificationDetails(
+      'channel-id', // This specifies the ID of the Notification
+      'Scheduled notification', // This specifies the name of the notification channel
+      'A scheduled notification', //This specifies the description of the channel
+      icon: 'icon',
+    );
+    // var iOSSpecifics = notifs.IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidSpecifics,
+      //  iOS: iOSSpecifics
+    );
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(
+        scheduledTime != null ? scheduledTime : DateTime.now(),
+        tz.local,
+      ),
+      platformChannelSpecifics,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    ); // This literally schedules the notification
   }
 }
