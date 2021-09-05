1477bd6dbf322f5f6fb17d40258fd171fb05ad4d
83wid <nabouzah@student.1337.ma>
2021-09-05 21:49:54 +0100

    init new alarmmanager and set scheduled tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:poopingapp/Controllers/medsController.dart';
 import 'package:poopingapp/Controllers/userController.dart';
 import 'package:poopingapp/screens/Setup_screens/setWaterScreen.dart';
 import 'package:poopingapp/screens/onboarding_screen.dart';
@@ -7,12 +9,121 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:poopingapp/screens/homeScreen.dart';
 import 'package:poopingapp/utilities/darkThemeProvider.dart';
+import 'package:poopingapp/utilities/notificationManager.dart';
 import 'package:provider/provider.dart';
-
 import 'utilities/styles.dart';
+import 'dart:isolate';
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 
-void main() {
+String name = 'portName';
+
+void waterRestState() async {
+if(DateTime.now().hour == 0){  final int isolateId = Isolate.current.hashCode;
+  await Firebase.initializeApp();
+  await UserController.createProp('waterDrank', '0').catchError((err) {
+    print({'message': err.message, 'id': isolateId});
+  });
+  await restTakesState().catchError((err) {
+    print(err.message);
+  });
+  print({'id': isolateId});}
+}
+
+void watercheckState() async {
+  final int isolateId = Isolate.current.hashCode;
+  if (DateTime.now().hour > 6) {
+    await Firebase.initializeApp();
+
+    final drunk = await UserController.getProp('waterDrank').catchError((err) {
+      print({'message': err.message, 'id': isolateId});
+    });
+    final amount =
+        await UserController.getProp('waterAmount').catchError((err) {
+      print({'message': err.message, 'id': isolateId});
+    });
+    if (drunk / amount < (DateTime.now().hour - 6) / 16)
+    {
+      final notif = NotificationManager();
+      notif.showNotificationDaily(
+              3,
+              'You should drink some water',
+              'you out of schedule',
+              DateTime.now().hour,
+              DateTime.now().minute + 1);
+        
+    }
+  }
+  print({'id': isolateId});
+}
+
+void checkTakesNotif() async {
+  await Firebase.initializeApp();
+  final int isolateId = Isolate.current.hashCode;
+  final meds = await UserController.getProp('medicine');
+  final takes = await UserController.getProp('medicineTakes');
+  final notif = NotificationManager();
+  if (takes.length > 0) {
+    int i = 0;
+    takes.forEach((key, value) {
+      value.forEach((key1, value1) {
+        if (compareTime(value1['time']) && value1['taken'] == '0') {
+          print(meds[i.toString()]['medicineName'] +
+              ' take at ' +
+              value1['time'] +
+              ' Is running late');
+          notif.showNotificationDaily(
+              i + int.parse(key1),
+              'Medicine running late',
+              meds[i.toString()]['medicineName'] +
+                  ' take at ' +
+                  value1['time'] +
+                  ' Is running late',
+              DateTime.now().hour,
+              DateTime.now().minute + 1);
+        }
+      });
+      i++;
+    });
+  }
+  print({'isolateId': isolateId});
+}
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  await AndroidAlarmManager.initialize();
+  await AndroidAlarmManager.periodic(
+    const Duration(minutes: 30),
+    2,
+    checkTakesNotif,
+    startAt: DateTime(DateTime.now().year, DateTime.now().month,
+        DateTime.now().day, DateTime.now().hour, 0),
+    exact: true,
+    rescheduleOnReboot: true,
+    wakeup: true,
+  );
+  await AndroidAlarmManager.periodic(
+    const Duration(minutes: 30),
+    1,
+    waterRestState,
+    exact: true,
+    rescheduleOnReboot: true,
+    wakeup: true,
+  ).catchError((err) {
+    print(err.message);
+  });
+  await AndroidAlarmManager.periodic(
+    const Duration(hours: 1),
+    66,
+    watercheckState,
+    exact: true,
+    rescheduleOnReboot: true,
+    wakeup: true,
+  ).catchError((err) {
+    print(err.message);
+  });
+ 
   runApp(ChangeNotifierProvider<DarkThemeProvider>(
     create: (_) => new DarkThemeProvider(),
     child: App(),
@@ -33,6 +144,13 @@ class _AppState extends State<App> {
     try {
       // Wait for Firebase to initialize and set `_initialized` state to true
       await Firebase.initializeApp();
+      // await restTakesState().catchError((err) {
+      //   print(err.message);
+      // });
+      // await checkTakeState().catchError((err) {
+      // print(err.message);
+      // });
+
       setState(() {
         _initialized = true;
       });
@@ -81,8 +199,8 @@ class _AppState extends State<App> {
 }
 
 Widget toshow() {
-final currentUserid = FirebaseAuth.instance.currentUser?.uid;
-CollectionReference users = FirebaseFirestore.instance.collection('users');
+  final currentUserid = FirebaseAuth.instance.currentUser?.uid;
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
   return FutureBuilder(
     future: users.doc(currentUserid).get(),
     builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -94,9 +212,9 @@ CollectionReference users = FirebaseFirestore.instance.collection('users');
         return SetWaterScreen();
       }
       if (snapshot.connectionState == ConnectionState.done) {
-        if (snapshot.data['completed'] == 'true')
-          return MyHomePage(title: snapshot.data['medicine']['medicineTakes']);
-        if (snapshot.data['completed'] == 'false') {
+        if (snapshot.data!['completed'] == 'true')
+          return MyHomePage(title: 'shit');
+        if (snapshot.data!['completed'] == 'false') {
           return SetWaterScreen();
         }
       }
