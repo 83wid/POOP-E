a51a8f1a14b6b84425d42e39c0bbb47a87074de7
83wid <nabouzah@student.1337.ma>
2021-09-05 21:46:19 +0100

    added routes

--- lib/utilities/charts.dart
+++ lib/utilities/charts.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:poopingapp/Controllers/userController.dart';
-import 'package:poopingapp/utilities/styles.dart';
+import 'package:poopingapp/screens/medsListing.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class WaterChart extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -14,7 +13,7 @@ class WaterChart extends StatelessWidget {
             final drank = double.parse(snapshot.data![1]);
             final still = amount - drank;
             final List<_WaterData>? waterData = [
-              _WaterData('To drink', still, '$still L'),
+              _WaterData('To drink', still, still.toStringAsFixed(2) + ' L'),
               _WaterData('Drank', drank, '$drank L'),
             ];
             return Center(
@@ -52,7 +51,7 @@ Future<List<String>> getWaterprops() async {
   final String? amount = await UserController.getProp('waterAmount');
   data[0] = amount != null ? amount : '';
   data[1] = drank != null ? drank : '';
-  print(data);
+  // print(data);
   return data;
 }
 
@@ -63,59 +62,44 @@ class _WaterData {
   final String? text;
 }
 
+final List<String> img = [
+  'assets/images/medstake.png',
+  'assets/images/medsTaken.png',
+  'assets/images/medsNtaken.png',
+];
+
 class MedsChart extends StatelessWidget {
-  final List<String> img = [
-    'assets/images/medsTaken.png',
-    'assets/images/medsNtaken.png',
-  ];
   final data = [
     _SalesData('22', 12.0),
     _SalesData('22', 2.0),
   ];
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Text(
-          'Your meds Today',
-          style: TextStyle(fontFamily: 'Bebas_Neue', fontSize: 20),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            // color: Color(0xFF4f3324),
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Color(0xFF4f3324),
-            ),
-          ),
-          height: MediaQuery.of(context).size.height / 6.4,
-          child: Expanded(
-            flex: 1,
-            child: FutureBuilder(
-                future: UserController.getAllProp(),
-                builder: (context, AsyncSnapshot<Users?> snapshot) {
-                  if (snapshot.data != null) {
-                    final Map<String, dynamic> takesData =
-                        snapshot.data!.medicineTakes;
-                    return ListView.builder(
-                        itemCount: takesData.length,
-                        itemBuilder: (context, index) {
-                          return takeitem(
-                              context, takesData[index.toString()], img);
-                        });
-                  }
-                  return Container();
-                }),
-          ),
-        ),
-      ],
-    ));
+    return FutureBuilder(
+        future: UserController.getAllProp(),
+        builder: (context, AsyncSnapshot<Users> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.data != null) {
+            final Map<String, dynamic> takesData = snapshot.data!.medicineTakes;
+            if (takesData.length > 0) {
+              // print('tkaes: ');
+              // print(takesData.length);
+              return Container(
+                width: MediaQuery.of(context).size.width / 6.4,
+                child: ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: generate(
+                        context, takesData, img, snapshot.data!.medicine)),
+              );
+            }
+          }
+          return Container();
+        });
   }
 }
 
 Widget takeitem(context, Map<String, dynamic> item, List<String> img) {
-  final String imgPath = item['taken'] != 'false' ? img[0] : img[1];
+  final String imgPath = img[int.parse(item['taken'])];
   return Container(
     decoration: BoxDecoration(
       color: Color(0xFF4f3324),
@@ -136,7 +120,7 @@ Widget takeitem(context, Map<String, dynamic> item, List<String> img) {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Image.asset(imgPath,
-            width: MediaQuery.of(context).size.width / 12, fit: BoxFit.contain),
+            width: MediaQuery.of(context).size.width / 15, fit: BoxFit.contain),
         Text(item['time']),
       ],
     ),
@@ -148,4 +132,55 @@ class _SalesData {
 
   final String year;
   final double sales;
+}
+
+List<Widget> generate(context, takesData, img, medsdata) {
+  return new List.generate(takesData.length, (index) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => MedsListScreen(
+                      data: medsdata[index.toString()],
+                      medId: index.toString(),
+                    )));
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(''),
+          RichText(
+            text: TextSpan(
+                text: '',
+                style: TextStyle(fontFamily: 'Bebas_Neue', fontSize: 16),
+                children: [
+                  TextSpan(
+                      text: medsdata[index.toString()]['medicineName'],
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).highlightColor)),
+                  TextSpan(
+                    text: ' Takes for Today',
+                  ),
+                ]),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 2.1,
+            height: MediaQuery.of(context).size.height / 6.5,
+            child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                itemCount: takesData[index.toString()].length,
+                itemBuilder: (context, idex) {
+                  // print(takesData[idex.toString()]);
+                  return takeitem(context,
+                      takesData[index.toString()][idex.toString()], img);
+                }),
+          ),
+        ],
+      ),
+    );
+  });
 }
