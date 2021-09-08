729a6d2d14cbc9c0476700f1a304c2fa003a9f80
83wid <nabouzah@student.1337.ma>
2021-09-08 01:33:45 +0100

    now displays meds and water results along with bowl in each day

--- lib/utilities/monthCalendar.dart
+++ lib/utilities/monthCalendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poopingapp/Controllers/medsController.dart';
 import 'package:poopingapp/Controllers/userController.dart';
 import 'package:poopingapp/utilities/styles.dart';
 
@@ -16,7 +17,7 @@ List daysOfWeek = [
 
 Widget monthCalander(context) {
   return FutureBuilder<dynamic>(
-      future: UserController.getProp('bowlEntries'),
+      future: UserController.getAllProp(),
       builder: (context, snapshot) {
         if (snapshot.data != null) {
           return Column(
@@ -28,7 +29,7 @@ Widget monthCalander(context) {
       });
 }
 
-Widget weekCalander(context, i, Map<String, dynamic> data) {
+Widget weekCalander(context, i, data) {
   return i == 0
       ? Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -53,7 +54,7 @@ Widget weekCalander(context, i, Map<String, dynamic> data) {
               7, (index) => dayCalander(context, index, i - 1, data)));
 }
 
-Widget dayCalander(context, int index, int weekid, Map<String, dynamic> data) {
+Widget dayCalander(context, int index, int weekid, data) {
   final today = DateTime.now();
   int month = today.month;
   final firstDay = DateTime(today.year, today.month, 1);
@@ -71,15 +72,12 @@ Widget dayCalander(context, int index, int weekid, Map<String, dynamic> data) {
     month = month != 12 ? month + 1 : 1;
     day = 7 * weekid + index - dayspermonth[month] - 1;
   }
-  final dayData = data[DateTime(today.year, month, day)
+  final daybowlData = data.bowlEntries[DateTime(today.year, month, day)
       .toString()
       .substring(0, today.toString().indexOf(' '))];
-  if (dayData != null) {
-    imgId = dayData.entries.first.value['type'];
-    // .forEach((key, value) {
-    // return (int.parse(value['type']) + 1).toString();
-    // });
-    print(dayData.entries.first.value['type']);
+  if (daybowlData != null) {
+    imgId = daybowlData.entries.first.value['type'];
+    print(daybowlData.entries.first.value['type']);
   }
   return Container(
     decoration: BoxDecoration(
@@ -93,46 +91,120 @@ Widget dayCalander(context, int index, int weekid, Map<String, dynamic> data) {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Row(children: [
-          Text(day.toString()),
+          Text(day.toString(),
+              style: TextStyle(
+                color: month == today.month ? Colors.white : Colors.grey,
+              )),
         ]),
-        dayData != null
-            ? Container(
-                padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width / 100),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      // color: Colors.brown[100],
-                      child: Image.asset('assets/images/type${int.parse(imgId) + 1}.png',
-                          width: MediaQuery.of(context).size.width / 20,
-                          fit: BoxFit.contain),
-                    ),
-                    dayData.length > 1
-                        ? Container(
-                            width: MediaQuery.of(context).size.width / 30,
-                            height: MediaQuery.of(context).size.width / 30,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                              color: Colors.green,
-                            ),
-                            child: Center(
-                                child: Text(
-                                    '+' + (dayData.length - 1).toString(),
-                                    style: TextStyle(fontSize: 10))))
-                        : SizedBox(height: 0, width: 0)
-                  ],
-                ),
-              )
-            : Text(''),
-        dayData != null
-            ? Container(
-                color: Colors.green,
-                width: MediaQuery.of(context).size.width / 10,
-                height: MediaQuery.of(context).size.width / 200,
-              )
-            : SizedBox(height: 0),
+        Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width / 100),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              bowlData(context, daybowlData, imgId),
+              moreBowlToday(context, daybowlData),
+              waterData(context, data, today, month, day),
+              dayMeds(context, data, today, month, day)
+            ],
+          ),
+        ),
+        dayIndicator(context, data, daybowlData, today, month, day)
       ],
     )),
   );
+}
+
+Widget waterData(context, allData, today, month, day) {
+  final data = allData.waterDrank[DateTime(today.year, month, day)
+      .toString()
+      .substring(0, today.toString().indexOf(' '))];
+  if (data != null &&
+      DateTime(today.year, month, day) !=
+          DateTime(today.year, today.month, today.day)) {
+    final target = double.parse(data['waterTarget']);
+    final drunk = double.parse(data['waterDrunk']);
+    final image = target - drunk <= 0.05
+        ? 'assets/images/waterTargetMet.png'
+        : 'assets/images/waterTargetNmet.png';
+    return Image.asset(image,
+        width: MediaQuery.of(context).size.width / 35, fit: BoxFit.contain);
+  }
+  return SizedBox(height: 0, width: 0);
+}
+
+Widget bowlData(context, daybowlData, imgId) {
+  if (daybowlData != null) {
+    return Container(
+      // color: Colors.brown[100],
+      child: Image.asset('assets/images/type${int.parse(imgId) + 1}.png',
+          width: MediaQuery.of(context).size.width / 25, fit: BoxFit.contain),
+    );
+  }
+  return SizedBox(height: 0, width: 0);
+}
+
+Widget moreBowlToday(context, daybowlData) {
+  if (daybowlData != null && daybowlData.length > 1) {
+    return Container(
+        width: MediaQuery.of(context).size.width / 35,
+        height: MediaQuery.of(context).size.width / 35,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: Colors.green,
+        ),
+        child: Center(
+            child: Text('+' + (daybowlData.length - 1).toString(),
+                style: TextStyle(fontSize: 8))));
+  }
+  return SizedBox(height: 0, width: 0);
+}
+
+Widget dayMeds(context, allData, today, month, day) {
+  if (month < today.month || (month == today.month && today.day > day)) {
+    final medsData = allData.medicineTakesEntries[
+        DateTime(today.year, month, day)
+            .toString()
+            .substring(0, today.toString().indexOf(' '))];
+    final image = checkMedStatus(medsData)
+        ? 'assets/images/medsNtaken.png'
+        : 'assets/images/medsTaken.png';
+    if (medsData != null)
+      return Image.asset(image,
+          width: MediaQuery.of(context).size.width / 35, fit: BoxFit.contain);
+  }
+  return SizedBox(height: 0, width: 0);
+}
+
+Widget dayIndicator(context, allData, daybowlData, today, month, day) {
+  if (month < today.month || (month == today.month && today.day > day)) {
+    final waterData = allData.waterDrank[DateTime(today.year, month, day)
+        .toString()
+        .substring(0, today.toString().indexOf(' '))];
+    int count = 0;
+    final medsData = allData.medicineTakesEntries[
+        DateTime(today.year, month, day)
+            .toString()
+            .substring(0, today.toString().indexOf(' '))];
+    final target =
+        waterData != null ? double.parse(waterData['waterTarget']) : 2;
+    final drunk = waterData != null ? double.parse(waterData['waterDrunk']) : 0;
+    final color = month == today.month
+        ? [Colors.green, Colors.yellow, Colors.orange, Colors.red]
+        : [
+            Color(0xff1ea714),
+            Color(0xffa2a714),
+            Color(0xffa77514),
+            Color(0xffa71414)
+          ];
+    count += daybowlData == null ? 1 : 0;
+    count += waterData == null || target > drunk + 0.05 ? 1 : 0;
+    count += /*medsData == null || */ checkMedStatus(medsData) ? 1 : 0;
+    return Container(
+      color: color[count],
+      width: MediaQuery.of(context).size.width / 10,
+      height: MediaQuery.of(context).size.width / 200,
+    );
+  }
+  return SizedBox(height: 0, width: 0);
 }
