6973342befb0aab54b0f093f6221f4af22294265
83wid <nabouzah@student.1337.ma>
2021-09-08 01:35:41 +0100

    added icon to notification

--- lib/utilities/notificationManager.dart
+++ lib/utilities/notificationManager.dart
@@ -22,7 +22,7 @@ class NotificationManager {
   void initNotifications() {
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     var initializationSettingsAndroid =
-        new AndroidInitializationSettings('@mipmap/ic_launcher');
+        new AndroidInitializationSettings('@mipmap/launcher_icon');
     var initializationSettingsIOS = IOSInitializationSettings(
         onDidReceiveLocalNotification: onDidReceiveLocalNotification);
 
